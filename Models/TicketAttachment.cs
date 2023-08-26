56acd5cf3560e82dfdd91647abc06c7d880e1407
OSERHIRDEV <oserhir10@gmail.com>
2023-08-26 14:18:57 +0100

    TicketAttachment

---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBugTracker.Models
+{
+    public class TicketAttachment
+    {
+
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [DisplayName("File Description")]
+        public string Description { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+        public Byte[] FileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string FileContentType { get; set; }
+
+
+        // public virtual Ticket Ticket { get; set; }
+        public virtual BTUser user { get; set; }
+
+
+    }
+}
