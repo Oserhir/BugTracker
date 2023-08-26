dbf2e91de79cf146501072a2e3c0f69129eb6b04
OSERHIRDEV <oserhir10@gmail.com>
2023-08-26 13:46:35 +0100

    Ticket History

---
+++ Models/TicketHistory.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTracker.Models
+{
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Updated Item")]
+        public int Property { get; set; }
+
+        [DisplayName("Previous")]
+        public int OldValue { get; set; }
+
+        [DisplayName("Current")]
+        public int NewValue { get; set; }
+
+        [DisplayName("Date Modified")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Description of Change")]
+        public int Description { get; set; }
+
+        [DisplayName("Team Member")]
+        public int UserId { get; set; }
+
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser user { get; set; }
+
+    }
+}
