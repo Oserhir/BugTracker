681a87bf9e2b96a644ad8a5ee0bf6e6473a8f316
OSERHIRDEV <oserhir10@gmail.com>
2023-08-26 21:42:04 +0100

    Add Model Project

---
+++ Models/ProjectPriority.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTracker.Models
+{
+    public class ProjectPriority
+    {
+
+        public int Id { get; set; }
+
+        [DisplayName("Priority Name")]
+        public string Name { get; set; }
+
+
+    }
+}
