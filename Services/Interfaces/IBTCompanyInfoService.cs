2b2f3e748078af6bd1a47898566f1122991a681e
OSERHIRDEV <oserhir10@gmail.com>
2023-08-29 17:51:02 +0100

    add Company Interface

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,17 @@
+﻿using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTCompanyInfoService
+    {
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId);
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId);
+
+    }
+}
