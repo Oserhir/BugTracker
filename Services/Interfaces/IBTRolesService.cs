bdb1a6e1452d3fd4b19cc0dda973415e2b1daa44
OSERHIRDEV <oserhir10@gmail.com>
2023-08-29 13:10:13 +0100

    add Role Interfaces

---
+++ Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        // public Task<List<IdentityRole>> GetRolesAsync();
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+
+    }
+}
