74484ad6326f97d8fb7ce94947cc89db39c6ec34
OSERHIRDEV <oserhir10@gmail.com>
2023-09-01 20:41:19 +0100

    BTEmailService Class

---
+++ Services/BTEmailService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services
+{
+    public class BTEmailService : IEmailSender
+    {
+        private readonly MailSettings _emailSender;
+
+        public BTEmailService(IOptions<MailSettings> mailSettings)
+        {
+            _emailSender = mailSettings.Value;
+        }
+
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
