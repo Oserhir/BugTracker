c300436a4c717118e91515037673237e5d32389c
OSERHIRDEV <oserhir10@gmail.com>
2023-09-01 12:46:47 +0100

    add custom Service - BTTicketService to program.cs

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddIdentity<BTUser , IdentityRole>(options => options.SignIn.Re
 builder.Services.AddScoped<IBTRolesService, BTRolesService>();
 builder.Services.AddScoped<IBTCompanyInfoService, BTCompanyInfoService>();
 builder.Services.AddScoped<IBTProjectService, BTProjectService>();
+builder.Services.AddScoped<IBTTicketService, BTTicketService>();
 
 builder.Services.AddControllersWithViews();
 
