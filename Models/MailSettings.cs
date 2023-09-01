396aedfecc01a5103edf7a9566704a4303081cff
OSERHIRDEV <oserhir10@gmail.com>
2023-09-01 20:53:12 +0100

    SendEmailAsync

--- Models/MailSettings.cs
+++ Models/MailSettings.cs
@@ -6,6 +6,6 @@
         public string DisplayName { get; set; }
         public string Password { get; set; }
         public string Host { get; set; }
-        public string Port { get; set; }
+        public int Port { get; set; }
     }
 }
--- Services/BTEmailService.cs
+++ Services/BTEmailService.cs
@@ -1,22 +1,52 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
+using MimeKit;
 using TheBugTracker.Models;
 
 namespace TheBugTracker.Services
 {
     public class BTEmailService : IEmailSender
     {
-        private readonly MailSettings _emailSender;
+        private readonly MailSettings _mailSettings;
 
         public BTEmailService(IOptions<MailSettings> mailSettings)
         {
-            _emailSender = mailSettings.Value;
+            _mailSettings = mailSettings.Value;
         }
 
 
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            MimeMessage email = new();
+
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = htmlMessage
+            };
+
+            email.Body = builder.ToMessageBody();
+
+            try
+            {
+                using var smtp = new SmtpClient();
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+                await smtp.SendAsync(email);
+
+                smtp.Disconnect(true);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
