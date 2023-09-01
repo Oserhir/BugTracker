b492722cd9b7a7f872b4fbf15150037a0e122e16
OSERHIRDEV <oserhir10@gmail.com>
2023-09-01 13:13:07 +0100

    IBTTicketHistoryService Interface

---
+++ Services/Interfaces/IBTTicketHistoryService.cs
@@ -0,0 +1,18 @@
+﻿using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTTicketHistoryService
+    {
+
+        Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId);
+
+        Task AddHistoryAsync(int ticketId, string model, string userId);
+
+        Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId);
+
+        Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int compnayId);
+
+
+    }
+}
