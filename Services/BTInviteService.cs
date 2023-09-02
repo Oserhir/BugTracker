b9e6a305e328bbf359124b160a12d3808fab1731
OSERHIRDEV <oserhir10@gmail.com>
2023-09-02 11:43:57 +0100

    AcceptInviteAsync

---
+++ Services/BTInviteService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTInviteService : IBTInviteService
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public BTInviteService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
+
+            if(invite == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                invite.IsValid = false;
+                invite.InviteeId = userId;
+                await _context.SaveChangesAsync();
+
+                return true;
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+
+        }
+
+        public Task AddNewInviteAsync(Invite invite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(int inviteId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateInviteCodeAsync(Guid? token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
