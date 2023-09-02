4e755d20f291efd9343c2056c6cf85fdb0a6aadd
OSERHIRDEV <oserhir10@gmail.com>
2023-09-02 12:47:33 +0100

    ConvertFileToByteArrayAsync

--- Services/BTFileService.cs
+++ Services/BTFileService.cs
@@ -22,7 +22,21 @@ namespace TheBugTracker.Services
 
         public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
         {
-            throw new NotImplementedException();
+            try
+            {
+                MemoryStream memoryStream = new();
+                await file.CopyToAsync(memoryStream);
+                byte[] byteFile = memoryStream.ToArray();
+                memoryStream.Close();
+                memoryStream.Dispose();
+
+                return byteFile;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public string FormatFileSize(long bytes)
